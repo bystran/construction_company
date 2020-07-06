b87e278dbf93b4b5a8b66304c04d8b73ca110c3a
Adam Bystran <bystran.adam.21@gmail.com>
2020-07-06 16:55:05 +0200

    Adds google analytics and a cookie consent.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,15 @@ import router from './router'
 import store from './store'
 import Vuelidate from 'vuelidate'
 import Toasted from 'vue-toasted'
+import VueAnalytics from 'vue-analytics'//and then use it in main.js
+
 
 import './app.scss'
+
+Vue.use(VueAnalytics, {
+    id: 'UA-171598895-1',
+    router
+})
 
 Vue.use(Vuelidate)
 Vue.use(Toasted)
--- src/views/BaseView.vue
+++ src/views/BaseView.vue
@@ -2,20 +2,26 @@
     <div>
         <nav-bar></nav-bar>
         <main >
-            <router-view></router-view>
+            <router-view ></router-view>
         </main>
 
         <footer-cmp></footer-cmp>
+        <cookie-law theme="dark-lime" button-text="Rozumiem!">
+            <div slot='message'>
+                Používaním tejto stránky súhlasíte s používaním cookies. Táto stránka spracováva cookies pre analýzu návštevnosti.
+            </div>
+        </cookie-law>
     </div>
 </template>
 <script>
     import NavBar from '../components/NavBar';
 
     import FooterCmp from '../components/Footer';
+    import CookieLaw from 'vue-cookie-law';
 
     export default {
         name: "base-view",
-        components: {NavBar, FooterCmp},
+        components: {NavBar, FooterCmp, CookieLaw},
         data(){
             return {
                 isLoggedIn : null,
